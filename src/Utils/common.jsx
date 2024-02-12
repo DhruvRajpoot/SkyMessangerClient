022855b9525c01a69d661381a40077dd5efba405
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-02-12 17:41:51 +0530

    fix: date formatter function

--- src/Utils/common.jsx
+++ src/Utils/common.jsx
@@ -1,41 +1,31 @@
-export const formateDate = (date) => {
-  const d = new Date(date);
+const today = new Date();
 
-  // return today if date is today
-  if (d.getDate() === new Date().getDate()) {
+export const formateDate = (dateString) => {
+  const date = new Date(dateString);
+
+  date.setHours(0, 0, 0, 0);
+  today.setHours(0, 0, 0, 0);
+
+  const diffTime = Math.abs(today - date);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  if (diffDays === 0) {
     return "Today";
-  }
-
-  // return yesterday if date is yesterday
-  if (d.getDate() === new Date().getDate() - 1) {
+  } else if (diffDays === 1) {
     return "Yesterday";
+  } else if (diffDays <= 7) {
+    const options = { weekday: "long" };
+    return date.toLocaleDateString("en-US", options);
+  } else {
+    const options = { day: "2-digit", month: "short", year: "numeric" };
+    return date.toLocaleDateString("en-GB", options);
   }
-
-  // return monday , tuesday etc if date is this week
-  if (d.getDate() > new Date().getDate() - 7) {
-    const days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thrusday",
-      "Friday",
-      "Saturday",
-    ];
-    return days[d.getDay()];
-  }
-
-  // return date in dd-mm-yyyy format
-  const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-  const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-  const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-  return `${da}-${mo}-${ye}`;
 };
 
-export const formateTime = (date) => {
-  const d = new Date(date);
-  let hours = d.getHours();
-  let minutes = d.getMinutes();
+export const formateTime = (dateString) => {
+  const date = new Date(dateString);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
   let ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
@@ -44,6 +34,6 @@ export const formateTime = (date) => {
   return strTime;
 };
 
-export const formateDateAndTime = (date) => {
-  return `${formateDate(date).toLowerCase()} at ${formateTime(date)}`;
+export const formateDateAndTime = (dateString) => {
+  return `${formateDate(dateString)} at ${formateTime(dateString)}`;
 };
